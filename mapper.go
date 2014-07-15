48e3b1607c452bfddd99b8b5c7c8dbdeb1e92978
Arvind Kumar C <arvindkumar.c@gmail.com>
2014-07-15 15:19:06 +0530

    Pointer will be passed to mapper. Returns only error if any. <Arvind/RamS>

--- mapper.go
+++ mapper.go
@@ -6,27 +6,23 @@ func main() {
 
 }
 
-func getValueObject(v interface{}) reflect.Value {
-	return reflect.New(reflect.TypeOf(v)).Elem()
-}
-
-func fromFieldValues(v reflect.Value, index int) (reflect.Value, reflect.StructField, string){
+func fromFieldValues(v reflect.Value, index int) (reflect.Value, reflect.StructField, string) {
 	return v.Field(index), v.Type().Field(index), v.Type().Field(index).Name
 }
 
-func AutoMap(from interface{}, to interface{}) (interface{}, error) {
+func AutoMap(from interface{}, to interface{}) error {
 
 	fromVal := reflect.ValueOf(from)
-	toVal := getValueObject(to)
+	toVal := reflect.ValueOf(to).Elem()
 
 	for index := 0; index < fromVal.NumField(); index++ {
 
 		fromField, fromFieldType, fromFieldTypeName := fromFieldValues(fromVal, index)
 		toFieldType, toFieldExist := toVal.Type().FieldByName(fromFieldTypeName)
-		
+
 		if toFieldExist && fromFieldType.Type == toFieldType.Type {
-				toVal.FieldByName(fromFieldTypeName).Set(fromField)
+			toVal.FieldByName(fromFieldTypeName).Set(fromField)
 		}
 	}
-	return toVal.Interface(), nil
+	return nil
 }
